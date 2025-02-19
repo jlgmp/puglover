f43f6dea8c49f633bd8f2ad917455a3a94826640
YAO SHIYAN <121219095+heavensighs@users.noreply.github.com>
2025-02-19 09:59:41 +0000

    1

--- data_generate.py
+++ data_generate.py
@@ -1,26 +1,34 @@
 import datetime
 import random
 
+
 start_date = datetime.date(2024, 11, 11)  
-end_date = datetime.date(2025, 2, 19)  
+end_date = datetime.date.today() - datetime.timedelta(days=1)
 
 
+device_ids = set()  
+while len(device_ids) < 20:
+    part1 = str(random.randint(100, 999))  
+    part2 = str(random.randint(100, 999))
+    part3 = str(random.randint(100, 999))
+    device_id = f"{part1}-{part2}-{part3}"
+    device_ids.add(device_id)  
+
+with open("userDatabase.txt", "w") as f:
+    for device_id in device_ids:
+        f.write(device_id + "\n")
 
 
-with open('meterDatabase.txt','w',encoding='utf-8') as f:
+with open('meterDatabase.txt','w') as f:
     f.write("DeviceID,Date,Final_Daily_Readings,Daily_Consumption\n")
-    for i in range(1,10):
-        device=f"{i}{i}{i}"+'-'+f"{i}{i}{i}"+'-'+f'{i}{i}{i}'
+    for device in device_ids:
         current_date = start_date
-        read=random.uniform(100.1,1000.4)
-        read=round(read,1)
+        read = round(random.uniform(100.1, 1000.4), 1)  
+
         while current_date <= end_date:
-        
             date = current_date.strftime('%Y-%m-%d')
-            consumption=random.uniform(4.1,16.6)
-            consumption=round(consumption,1)
-            read+=consumption
-            
+            consumption = round(random.uniform(4.1, 16.6), 1)  
+            read += consumption  
             
             f.write(f"{device},{date},{read:.1f},{consumption}\n")
             current_date += datetime.timedelta(days=1)
