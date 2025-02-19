036c99e642e54981b5ecce889a52b0256ec1d06f
Panda-the-Pooh <WANG2215@e.ntu.edu.sg>
2025-02-19 18:51:18 +0000

    111

--- app.py
+++ app.py
@@ -10,47 +10,7 @@ logging.basicConfig(filename='electricity.txt', level=logging.INFO,
                     format='%(asctime)s - %(message)s')
 
 acceptAPI=True
-# --------------------------------- Register ---------------------------------------------
-'''
-# Define Class User
-global user_database
-user_database=[]
 
-class User:
-    def __init__(self,userID):
-        self.userID=userID
-        self.device_id_list=[]
-    def add_device(self,device_id):
-        self.device_id_list.append(device_id)
-    def get_device_id(self):
-        return self.device_id_list
-    def __str__(self):
-        return f"User:{self.userID} device_list:{self.device_id_list}"
-
-# Define Register Function
-@app.route('/register',methods=['POST'])
-def register():
-    global user_database
-    data = request.get_json()
-    userID = data.get('userID')
-    device_id=data.get('deviceID')
-    if not user_database:
-        user=User(userID)
-        user.add_device(device_id)
-        user_database.append(user)
-    if not any(user.userID==userID for user in user_database):
-        user=User(userID)
-        user.add_device(device_id)
-        user_database.append(user)
-    else:
-        for user in user_database:
-            if user.userID==userID and device_id not in user.get_device_id():
-                user.add_device(device_id)
-                break
-    for i in user_database:
-        print(i)
-    return  jsonify({"status": "success", "message": f"device id:{device_id} registered successfully."})
-'''
 
 # ----------------------------------- Meter Reading ---------------------------------------------
 
@@ -95,14 +55,6 @@ def meter_data():
 
 # --------------------------------- Backup Define ---------------------------------------------
 
-# User Data Backup
-def userDataBackUp(user_database):
-    with open('userDatabase.txt', 'w', encoding='utf-8') as f:
-        for user in user_database:
-            userstr=user.userID+','+",".join(user.get_device_id())
-            f.write(userstr+'\n')
-    print("Backup completed!") 
-
 # Meter Readings Backup
 def meterDataBackup(meter_database):
     df = pd.read_csv('meterDatabase.txt', sep=',', encoding='utf-8')
@@ -127,6 +79,9 @@ def meterDataBackup(meter_database):
     
     print("Backup completed!")
 
+    # meter_database.clear()
+    # print("Memory cleared for the next day's data collection.")
+
 
 # ----------------------------------- Stop Server ---------------------------------------------
 @app.route('/stopServer',methods=['GET', 'POST'])
@@ -138,7 +93,69 @@ def stop_server():
     return "Server Shutting Down"
 
 
-# --------------------------------- Recover Define ---------------------------------------------
+# ----------------------------------- Run ---------------------------------------------
+if __name__ == '__main__':
+    app.run(debug=False)
+
+
+
+
+## Potential Try
+
+# Register
+'''
+# Define Class User
+global user_database
+user_database=[]
+
+class User:
+    def __init__(self,userID):
+        self.userID=userID
+        self.device_id_list=[]
+    def add_device(self,device_id):
+        self.device_id_list.append(device_id)
+    def get_device_id(self):
+        return self.device_id_list
+    def __str__(self):
+        return f"User:{self.userID} device_list:{self.device_id_list}"
+
+# Define Register Function
+@app.route('/register',methods=['POST'])
+def register():
+    global user_database
+    data = request.get_json()
+    userID = data.get('userID')
+    device_id=data.get('deviceID')
+    if not user_database:
+        user=User(userID)
+        user.add_device(device_id)
+        user_database.append(user)
+    if not any(user.userID==userID for user in user_database):
+        user=User(userID)
+        user.add_device(device_id)
+        user_database.append(user)
+    else:
+        for user in user_database:
+            if user.userID==userID and device_id not in user.get_device_id():
+                user.add_device(device_id)
+                break
+    for i in user_database:
+        print(i)
+    return  jsonify({"status": "success", "message": f"device id:{device_id} registered successfully."})
+'''
+
+# User Data Backup
+'''
+def userDataBackUp(user_database):
+    with open('userDatabase.txt', 'w', encoding='utf-8') as f:
+        for user in user_database:
+            userstr=user.userID+','+",".join(user.get_device_id())
+            f.write(userstr+'\n')
+    print("Backup completed!")
+'''
+
+# Recover
+'''
 def userDataRecover(user_database):
     with open('userDatabase.txt', 'r', encoding='utf-8') as f: 
         for line in f:
@@ -149,13 +166,6 @@ def userDataRecover(user_database):
             for i in data:
                 user.add_device(i)
             user_database.append(user)
-'''def eleDataRecover(meter_database):
+def eleDataRecover(meter_database):
     with open('meterDatabase.txt','r',encoding='utf-8') as f:
-        '''
-
-
-
-
-# ----------------------------------- Run ---------------------------------------------
-if __name__ == '__main__':
-    app.run(debug=False)
+'''
